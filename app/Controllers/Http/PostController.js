1810a3d781344479b769718dfe800b9033f60473
João Victor Rodrigues de Souza <joaovictorrdesouza1@gmail.com>
2019-10-08 23:19:32 -0300

    first commit

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,93 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Post = use('App/models/Post')
+
+/**
+ * Resourceful controller for interacting with posts
+ */
+class PostController {
+  /**
+   * Show a list of all posts.
+   * GET posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index () {
+    const posts = await Post.all()
+
+    return posts
+  }
+
+  /**
+   * Create/save a new post.
+   * POST posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request }) {
+    const data = request.only(['content'])
+    const post = await Post.create(data)
+
+    return post;
+  }
+
+  /**
+   * Display a single post.
+   * GET posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    const post = await Post.find(params.id)
+
+    return post
+  }
+
+
+  /**
+   * Update post details.
+   * PUT or PATCH posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request }) {
+    const data = request.only(['content'])
+    const post = await Post.find(params.id)
+
+    post.merge(data);
+
+    await post.save()
+
+    return post
+  }
+
+  /**
+   * Delete a post with id.
+   * DELETE posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    post = await Post.find(params.id)
+
+    await post.delete()
+  }
+}
+
+module.exports = PostController
